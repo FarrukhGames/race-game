738d29283ae435e7c1d6b20cd42e98f791d8f24b
Farrukh <FarrukhKhuseynov2011@gmail.com>
2022-11-17 18:38:37 +0100

    Сделали столкновение и рандом

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ let left = draw.line(30, 0, 30, HEIGHT).stroke({width: 10, color: "black"});
 let right = draw.line(WIDTH - 30, 0, WIDTH - 30, HEIGHT).stroke({width: 10, color: "black"});
 let center = draw.line(WIDTH / 2, 0, WIDTH / 2, HEIGHT).stroke({width: 5, color: "black", dasharray: 10});
 let car = draw.image("img/car.png").height("80px").width("40px").x(WIDTH / 2).y(HEIGHT - 80);
-let obstacle = draw.image("img/wall.png").width("100px").height("20px").x(WIDTH / 2 - 20).y(20);
+let obstacle = draw.image("img/wall.png").width("100px").height("20px").move(WIDTH / 2 - 20,20);
 let restartButton = document.querySelector(".restart-button");
 let stepX = 0;
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-let roadMove = setInterval(() => {
+function roadMove() {
     if (center.y() == 0) {
         center.dy(10);
     } else {
@@ -23,31 +22,26 @@ let roadMove = setInterval(() => {
     obstacle.dy(3);
     if (obstacle.y() > HEIGHT) {
         obstacle.y(-500);
-        obstacle.x(getRandomInt(32, WIDTH - 132));
+        let randomX = getRandomInt(32, WIDTH - parseInt(obstacle.width()) - 32);
+        obstacle.x(randomX);
     }
-    setTimeout(() => {
-        clearInterval(roadMove);
-    }, 2000);
+    if (obstacle.y() + parseInt(obstacle.height()) > car.y() && obstacle.x() + parseInt(obstacle.width()) > car.x() && obstacle.x() < car.x() + parseInt(car.width())) {
+        clearInterval(roadMoveInterval);
+    }
+}
+
+let roadMoveInterval = setInterval(() => {
+    roadMove();
 }, 10);
 
 restartButton.addEventListener("click", () => {
-    let restartRoadmove = setInterval(() => {
-        if (center.y() == 0) {
-            center.dy(10);
-        } else {
-            center.dy(-10);
-        }
-        car.dx(stepX);
-        obstacle.dy(3);
-        if (obstacle.y() > HEIGHT) {
-            obstacle.y(-500);
-            obstacle.x(getRandomInt(32, WIDTH - 132));
-        }
+    clearInterval(roadMoveInterval);
+    roadMoveInterval = setInterval(() => {
+        roadMove();
     }, 10);
 });
 
 document.addEventListener("keydown", (event) => {
-    console.dir(event)
     if (event.code == "ArrowRight") {
         stepX = 5;
     }
